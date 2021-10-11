a8ed7ac5423f2d1607877b3f1a0a1924c1194310
MasnyReef <piotrrachx@gmail.com>
2021-10-11 12:36:48 +0200

    pierwszy push

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       String launch, imie;
+
+       launch="Witaj";
+       imie="Piotrek";
+
+       System.out.println(String.format("%s posiadacz imienia: %s",launch,imie));
+
+       System.exit( 0);
+    }
+}
